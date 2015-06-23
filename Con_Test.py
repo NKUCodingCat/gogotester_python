ad9ae88bc67921ce03d9787fb94bac765f4ed469
NKUCodingcat <chenjingtao930@gmail.com>
2015-06-24 00:17:08 +0800

    Ver 0.8.5 α
    Update the way to check Network

--- Con_Test.py
+++ Con_Test.py
@@ -1,28 +1,17 @@
-import  socket, Socket_Test, IPy
-def GetAdd(Name, Type):
-	try:
-		J = socket.getaddrinfo(Name, 80, 0, 0, socket.IPPROTO_TCP)
-		K = [i for i in J if IPy.IP(i[4][0]).version()==Type]
-		if len(K) == 0:
-			raise socket.error, "No ipv%s Addr found in this find"%Type
-	except socket.error:
-		raise
-	except:
-		raise
-	else:
-		return K[0]
-def Conn(ip, port=80):
-	return Socket_Test.Socket_Test(ip, port)[0]
-def Con_Test(v4 = "www.baidu.com", v6 = "ipv6.baidu.com"):
+import  urllib2
+
+def Con_Test(v4 = "www.baidu.com", v6 = "ipv6.he.net"):
 	RES = {}
 	#v4
 	try:
-		RES["v4"] = Socket_Test.Socket_Test(GetAdd(v4, 4)[4][0], 80)[0]
+		RES["v4"] = True
+		urllib2.urlopen("https://%s"%v4, timeout = 5)
 	except:
 		RES["v4"] = False
 	#v6
 	try:
-		RES["v6"] = Socket_Test.Socket_Test(GetAdd(v6, 6)[4][0],80)[0]
+		RES["v6"] = True
+		urllib2.urlopen("https://%s"%v6, timeout = 5)
 	except:
 		RES["v6"] = False
 	return RES
