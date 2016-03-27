539ce96c341dc68e147c5807e8387b3913ae5588
NKUCodingCat <chenjingtao930@gmail.com>
2016-03-28 00:37:32 +0800

    嗷呜

--- src/Main_Gevent.py
+++ src/Main_Gevent.py
@@ -11,6 +11,10 @@ import sysconfig
 reload(sys).setdefaultencoding('UTF-8')
 sys.dont_write_bytecode = True
 
+sys.path += glob.glob('%s/*.egg' % os.path.dirname(os.path.abspath(__file__)))
+sys.path += [os.path.abspath(os.path.join(__file__, '../packages.egg/%s' % x)) for x in ('noarch', sysconfig.get_platform().split('-')[0])]
+sys.path += glob.glob('%s/*.zip' % os.path.dirname(os.path.abspath(__file__)))
+sys.path+=[os.path.dirname(os.path.abspath(__file__)), ]
 
 import gevent
 from gevent import coros
